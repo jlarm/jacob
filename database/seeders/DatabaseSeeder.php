b66c0dc55d2ec23482f9dd7fc050ec6266b92649
jlarm <117611704+jlarm@users.noreply.github.com>
2025-06-04 14:19:02 -0500

    feat: update page title to Jacob Lohr and remove unused styles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Player;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,24 @@ final class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Joe Lohr',
+            'email' => 'emailme@joelohr.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        Player::create([
+            'name' => 'Jacob Lohr',
+            'birthdate' => '2014-03-26',
+            'height' => 72,
+            'weight' => 96,
+            'city' => 'Island Lake',
+            'state' => 'IL',
+            'team' => 'McHenry Cobras',
+            'number' => 26,
+            'positions' => ['Pitcher', '3B'],
+            'bats' => 'Right',
+            'throws' => 'Right',
         ]);
     }
 }
